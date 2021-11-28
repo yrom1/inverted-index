10f33b724e114eea1bac4cbba58cac315c3ea1df
yrom1 <ryanm.inbox@gmail.com>
2021-11-28 02:25:18 -0500

    Inverted indexes!

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,66 @@
+import scala.io.Source
+import java.io._
+import scala.util.{Try, Success, Failure}
+
+object InvertedIndex {
+  // From http://cowa.github.io/2015/12/07/inverted-index-scala/
+  // Added some steps to understand it better!
+  // And output the file.
+  /**
+   * Build an inverted index from a file with this format:
+   *   doc0 w0 w1 ...
+   *   doc1 w2 w4 ...
+   * Each elements of a line is separated by whitespaces
+   *
+   * @param path Path to the file containing data to invert
+   *
+   * @return An inverted index
+   *   w0 -> doc0 doc2 ...
+   *   w1 -> doc0 doc5 ...
+   */
+  def apply(path: String): Map[String, Vector[String]] = { 
+    val file = Source.fromFile(path)
+    val lines = file.getLines().toList
+    file.close()
+
+    println(lines)
+    // List(document0.txt word0 word1 word5, document1.txt word0 word5 word7)
+    
+    println(lines.map(_.split(" ")).map(_.toList.toString))
+    // List([Ljava.lang.String;@69ee0da7, [Ljava.lang.String;@72e71723)
+
+    println(lines.map(_.split(" "))
+      .flatMap(x => x.drop(1).map(y => (y, x(0)))))
+    // List((word0,document0.txt), (word1,document0.txt), (word5,document0.txt), (word0,document1.txt), (word5,document1.txt), (word7,document1.txt))
+    
+    println(lines.map(_.split(" "))
+      .flatMap(x => x.drop(1).map(y => (y, x(0))))
+      .groupBy(_._1))
+    // HashMap(word5 -> List((word5,document0.txt), (word5,document1.txt)), word7 -> List((word7,document1.txt)), word1 -> List((word1,document0.txt)), word0 -> List((word0,document0.txt), (word0,document1.txt)))
+
+    println(lines.map(_.split(" "))
+      .flatMap(x => x.drop(1).map(y => (y, x(0))))
+      .groupBy(_._1)
+      .map(p => (p._1, p._2.map(_._2).toVector)))
+    // HashMap(word5 -> Vector(document0.txt, document1.txt), word7 -> Vector(document1.txt), word1 -> Vector(document0.txt), word0 -> Vector(document0.txt, document1.txt))
+    
+    lines.map(_.split(" "))
+      .flatMap(x => x.drop(1).map(y => (y, x(0))))
+      .groupBy(_._1)
+      .map(p => (p._1, p._2.map(_._2).toVector))
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val iiMap = InvertedIndex("input.txt")
+    println(iiMap)
+
+    val file = new File("output.txt")
+    val bw = new BufferedWriter(new FileWriter(file))
+    
+    for (line <- iiMap.mkString("\n")) { bw.write(line) }
+    bw.write("\n")
+    bw.close()
+  }
+}
